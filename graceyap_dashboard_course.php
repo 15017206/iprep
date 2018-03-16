74a9a0701be72f4a5d9d2ca8496c8a47b39459a6
15017206 <15017206@myrp.edu.sg>
2018-03-16 20:47:44 +0800

    update2(glenn)

--- graceyap_dashboard_course.php
+++ graceyap_dashboard_course.php
@@ -21,14 +21,14 @@ and open the template in the editor.
 
             <div class="form-group">
                 <div class="container">
-                    <button type="button" data-toggle="modal" data-target="#courses_only_modal2" class="btn btn-block btn-danger">Add New Courses</button>
+                    <button type="button" data-toggle="modal" data-target="#courses_only_modal_add" class="btn btn-block btn-danger">Add New Courses</button>
                 </div>
             </div>
 
             <p>List of available courses:</p>
             <!--Add some courses here-->
             <div class="list-group">
-                <a href="#" data-toggle="modal" data-target="#courses_only_modal2" data-target="#courses_modal" class="list-group-item list-group-item-action flex-column align-items-start">
+                <a href="#" data-toggle="modal" data-target="#courses_only_modal_modify" data-target="#courses_modal" class="list-group-item list-group-item-action flex-column align-items-start">
                     <div class="d-flex w-100 justify-content-between">
                         <h5 class="mb-1">Introduction to IoT & Embedded Systems</h5>
                         <small>IT Security</small>
@@ -40,12 +40,60 @@ and open the template in the editor.
                 </a>
             </div>
 
-            <!--Modal for courses only-->
-            <div class="modal fade" id="courses_only_modal2" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+            <!--Modal for adding courses-->
+            <div class="modal fade" id="courses_only_modal_add" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
                 <div class="modal-dialog modal-dialog-centered" role="document">
                     <div class="modal-content">
                         <div class="modal-header">
                             <h5 class="modal-title" id="exampleModalLongTitle">Add new courses</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            <div class="form-group">
+                                <label for="exampleFormControlInput1">Name of new course:</label>
+                                <input type="text" class="form-control" id="" placeholder="eg. Introduction to IoT">
+                            </div>
+                            <div class="form-group">
+                                <label for="exampleFormControlSelect1">Course Genre:</label>
+                                <select class="form-control" id="exampleFormControlSelect1">
+                                    <option>Programming</option>
+                                    <option>IT Security</option>
+                                    <option>Internet of Things</option>
+                                    <option>Cognitive Technology</option>
+                                    <option>Data Analytics</option>
+                                    <option>Tech Support</option>
+                                </select>
+                            </div>
+                            <div class="form-group">
+                                <label for="exampleFormControlInput1">Cost:</label>
+                                <div class="input-group">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text" id="basic-addon1">S$</span>
+                                    </div>
+                                    <input type="number" class="form-control" id="" placeholder="Please put in SGD, eg. SGD$0.06">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label for="exampleFormControlInput1">Course Provider:</label>
+                                <input type="text" class="form-control" id="" placeholder="eg. Coursera, Codeacademy">
+                            </div>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-primary">Save changes</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!--Modal for editing/modifying existing course-->
+            <div class="modal fade" id="courses_only_modal_modify" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLongTitle">Modify existing courses</h5>
                             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                 <span aria-hidden="true">&times;</span>
                             </button>
