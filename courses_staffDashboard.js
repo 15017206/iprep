6d52dae834f66988b68d85e2315b71cee3a0bc94
15004557 <H0peyur3gd7>
2018-03-18 01:19:42 +0800

    Displaying of courses dynamically

--- courses_staffDashboard.js
+++ courses_staffDashboard.js
@@ -5,6 +5,37 @@
  */
 $(document).ready(function () {
 
+    var printCourseDetails = "";
+    $.ajax({
+        type: "GET",
+        url: "http://localhost/iprep/webservices/getCourses.php",
+        cache: false,
+        dataType: "JSON",
+        success: function (response) {
+
+
+            for (i = 0; i < response.length; i++) {
+                printCourseDetails += "<a href='#' data-toggle='modal' data-target='#courses_only_modal_modify' data-target='#courses_modal' class='list-group-item list-group-item-action flex-column align-items-start'>"
+                        + "<div class='d-flex w-100 justify-content-between'>"
+                        + "<h5 class='mb-1'>" + response[i].name +"</h5>"
+                        + "<small>" + response[i].genre + "</small>"
+                        + "</div>"
+                        + " <div class='d-flex w-100'>"
+                        + " <span class='badge badge-warning'>S$" + response[i].cost +"</span>"
+                        + " </div>"
+                        + "  <small>"+ response[i].course_provider +"</small>"
+                        + " </a>";
+                
+                
+                $("#courseDetails").html(printCourseDetails);
+            }
+
+        },
+        error: function (obj, textStatus, errorThrown) {
+            console.log("Error " + textStatus + ": " + errorThrown);
+        }
+    });
+
 
 
     //this portion of the code deals with insertion of course details into the database
@@ -24,9 +55,9 @@ $(document).ready(function () {
                 dataType: "JSON",
                 success: function (data, textStatus)
                 {
-                   
-                     $('#courses_only_modal2').modal('hide');
-                     $('#formAddNewCourse')[0].reset();
+
+                    $('#courses_only_modal2').modal('hide');
+                    $('#formAddNewCourse')[0].reset();
 
                 },
                 error: function (obj, textStatus, errorThrown) {
