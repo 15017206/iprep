25a8c07f41cc1c46336d698eee77cdc78da3d33c
15017206 <15017206@myrp.edu.sg>
2018-03-18 17:31:25 +0800

    update2(glenn)

--- student_dashboard_oiip.php
+++ student_dashboard_oiip.php
@@ -25,17 +25,32 @@ and open the template in the editor.
                     <div class="form-group">
                         <div class="container">
                             <button type="button" class="btn btn-block btn-success">Apply for OIIP</button>
-                            <button type="button" class="btn btn-block btn-success">Request OIIP approval from IMDA</button>
-                            <button type="button" class="btn btn-block btn-info">Submit OIIP approval email</button>
                         </div>
                     </div>
                 </form>
 
-                
+
             </div>
-            
+
             <div class="alert alert-warning" role="alert">
-                <p style="text-align: center">No OIIP Assigned yet</p>
+                <p>OIIP assignment:</p>
+                <div class="list-group">
+                    <ul class="list-group">
+                        <li data-toggle="modal" data-target="#courses_modal" class="list-group-item list-group-item-action flex-column align-items-start">
+                            <div class="d-flex w-100 justify-content-between">
+                                <h5 class="mb-1">Translation of Braille to English Text</h5>
+                                <small>Tokushima, Nagano, Japan</small>
+                            </div>
+                            <p class="mb-1"></p>
+                            <span class="badge badge-warning">Funding Status: Applied</span>&nbsp;<span class="badge badge-warning">Job Status: Accepted</span>&nbsp;<span class="badge badge-warning">Funding source: YTP</span>
+                            <br/>
+                            <a href=""><span class="badge badge-info">Request OIIP approval from IMDA</span></a>
+                            <a href=""><span class="badge badge-info">Submit OIIP approval email</span></a>
+                            <br/>
+                            <small>$1200/month</small>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>
     </body>
