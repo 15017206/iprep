e4cf340407a3c952753f5c73135be90b86c2d2d2
15017206 <15017206@myrp.edu.sg>
2018-03-18 15:33:31 +0800

    update2(glenn)

--- login.php
+++ login.php
@@ -13,27 +13,28 @@ and open the template in the editor.
     <body>
         <br/>
         <div class="container">
-            <div class="alert alert-dark" role="alert">iPrep Login</div>
-        </div>
-        <div class="container">
-            <form method="post" action="graceyap_dashboard.php">
-                <div class="form-group">
-                    <label for="exampleInputEmail1">Enter ID</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="eg. 15017206, 14003424">
-                    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-                </div>
-                <div class="form-group">
-                    <label for="exampleInputPassword1">Password</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="eg. xxxxxx">
-                </div>
-                <div class="form-check form-group">
-                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                    <label class="form-check-label" for="exampleCheck1">Remember me</label>
-                </div>
-                <div class="container-fluid">
-                    <button type="submit" class="btn btn-block btn-primary ">Submit</button>
-                </div>
-            </form>
+            <div class="alert alert-dark" role="alert">
+                <h5 style="text-align: center">iPrep Login</h5>
+
+                <form method="post" action="graceyap_dashboard.php">
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">Enter ID</label>
+                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="eg. 15017206, 14003424">
+                        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputPassword1">Password</label>
+                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="eg. xxxxxx">
+                    </div>
+                    <div class="form-check form-group">
+                        <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                        <label class="form-check-label" for="exampleCheck1">Remember me</label>
+                    </div>
+                    <div class="container-fluid">
+                        <button type="submit" class="btn btn-block btn-primary ">Submit</button>
+                    </div>
+                </form>
+            </div>
         </div>
     </body>
 </html>
