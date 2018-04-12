bba6c08f7d000c5b64be660adf28db97d4d67733
15017206 <15017206@myrp.edu.sg>
2018-04-13 00:19:05 +0800

    Merge origin/master
    Conflicts:
    	product_page.php

--- webservices/getUnallocatedVacancies.php
+++ webservices/getUnallocatedVacancies.php
@@ -2,7 +2,9 @@
 
 include 'dbconn.php';
 
-$query = "SELECT * FROM oiip_vacancy INNER JOIN company ON oiip_vacancy.company_id=company.company_id WHERE oiip_vacancy.vacancy_id NOT IN (SELECT vacancy_id FROM student_oiip_assignment)";
+$query = "SELECT * FROM company 
+INNER JOIN oiip_vacancy ON company.company_id=oiip_vacancy.company_id
+WHERE oiip_vacancy.vacancy_id NOT IN (SELECT vacancy_id FROM student_oiip_assignment)";
 
 $result = mysqli_query($link, $query) or die('Error querying database');
 
