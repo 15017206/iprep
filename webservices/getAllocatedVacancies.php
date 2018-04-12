e7a2fdc8cb187409be95f7b31b373dde25b7929f
15017206 <15017206@myrp.edu.sg>
2018-04-12 21:39:52 +0800

    Merge origin/master
    Conflicts:
    	product_page.php

--- webservices/getAllocatedVacancies.php
+++ webservices/getAllocatedVacancies.php
@@ -2,7 +2,7 @@
 
 include 'dbconn.php';
 
-$query = "SELECT * FROM oiip_vacancy INNER JOIN company ON oiip_vacancy.company_id=company.company_id WHERE oiip_vacancy.vacancy_id IN (SELECT vacancy_id FROM student_oiip_assignment)";
+$query = "SELECT * FROM oiip_vacancy INNER JOIN company ON oiip_vacancy.company_id=company.company_id INNER JOIN student_oiip_assignment ON oiip_vacancy.vacancy_id=student_oiip_assignment.vacancy_id INNER JOIN student ON student_oiip_assignment.student_id=student.student_id WHERE oiip_vacancy.vacancy_id IN (SELECT vacancy_id FROM student_oiip_assignment)";
 
 $result = mysqli_query($link, $query) or die('Error querying database');
 
---
+++ webservices/getAllocatedVacanciesv2.php
@@ -0,0 +1,20 @@
+<?php
+
+include 'dbconn.php';
+
+$query = "SELECT * FROM company 
+INNER JOIN oiip_vacancy ON company.company_id=oiip_vacancy.company_id
+INNER JOIN student_oiip_assignment ON oiip_vacancy.vacancy_id=student_oiip_assignment.vacancy_id
+INNER JOIN student ON student_oiip_assignment.student_id=student.student_id
+WHERE oiip_vacancy.vacancy_id IN (SELECT vacancy_id FROM student_oiip_assignment)";
+
+$result = mysqli_query($link, $query) or die('Error querying database');
+
+while ($row = mysqli_fetch_assoc($result)) {
+    $vacancies[] = $row;
+}
+mysqli_close($link);
+
+echo json_encode($vacancies);
+
+?>
